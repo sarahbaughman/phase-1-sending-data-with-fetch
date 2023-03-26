240d540ff9d872c9992dfdf8af7976292b886b1c
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-26 19:58:00 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+
+    let bodyURL = "http://localhost:3000/users"
+
+    function submitData (name, email){
+        return fetch(bodyURL, {
+            method: 'POST',
+        
+            headers: {
+                "Content-Type": "application/json",
+                "Accept" : "application/json",
+            },
+
+            body: JSON.stringify({
+                name: name,
+                email: email
+            })
+        })
+      
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            //let text = document.createElement("p")
+            //text.innerText = object["id"]
+            document.body.innerHTML = object["id"]
+        })
+
+        .catch(function (error) {
+          
+            document.body.innerHTML = error.message
+          });
+    }
+
+    submitData("Sarah","sarah@gmail.com")
